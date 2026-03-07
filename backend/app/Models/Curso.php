ca778e1c242051525ce73d1a93075bcf4a7da104
Knuto1603 <108632698+Knuto1603@users.noreply.github.com>
2026-03-07 00:01:51 -0500

    feat(Curso): add requisitos and prerequisitoDe BelongsToMany relations

--- backend/app/Models/Curso.php
+++ backend/app/Models/Curso.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Curso extends Model
@@ -26,5 +27,31 @@ class Curso extends Model
     public function planEstudios(): HasMany
     {
         return $this->hasMany(PlanEstudios::class);
+    }
+
+    /**
+     * Cursos que son requisito previo para este curso
+     */
+    public function requisitos(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            Curso::class,
+            'curso_requisitos',
+            'curso_id',
+            'requisito_curso_id'
+        );
+    }
+
+    /**
+     * Cursos que tienen este curso como requisito
+     */
+    public function prerequisitoDe(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            Curso::class,
+            'curso_requisitos',
+            'requisito_curso_id',
+            'curso_id'
+        );
     }
 }
